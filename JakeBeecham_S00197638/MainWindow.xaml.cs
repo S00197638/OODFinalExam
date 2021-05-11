656a41fc12c21f19fd67d47d46cee05bc6d0ab5a
Jake Beecham <60218222+JakeBeecham@users.noreply.github.com>
2021-05-11 11:47:33 +0100

    OODFinalExam Part 7 Complete S00197638.

--- JakeBeecham_S00197638/MainWindow.xaml.cs
+++ JakeBeecham_S00197638/MainWindow.xaml.cs
@@ -22,6 +22,7 @@ namespace JakeBeecham_S00197638
     {
         #region Setup
         GameData db;//Reference to Database
+        List<Game> AllGames;//List of all Games
 
         public MainWindow()
         {
@@ -31,11 +32,19 @@ namespace JakeBeecham_S00197638
         private void Window_Loaded(object sender, RoutedEventArgs e)
         {
             db = new GameData();//Getting Access to Database
+            AllGames = new List<Game>();//Creating new list
+
+            //Populate Combo Box
+            string[] filterTypes = { "All", "PC", "PS", "Switch", "Xbox" };
+            cbGameFilter.ItemsSource = filterTypes;
+            cbGameFilter.SelectedItem = "All";
 
             var query = db.Games
                 .Select(g => g);
 
-            lbxGames.ItemsSource = query.ToList();
+            AllGames = query.ToList();
+
+            lbxGames.ItemsSource = AllGames;
         }
         #endregion
 
@@ -50,6 +59,73 @@ namespace JakeBeecham_S00197638
                     $"Platform(s): {selectedGame.Platform}";
                 tblkGameDescription.Text = $"Game Description: {selectedGame.Description}";
             }
+        }
+        #endregion
+
+        #region Filter
+        private void cbGameFilter_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            //Determine what is selected
+            string selectedFilter = cbGameFilter.SelectedItem as string;
+            //Setup Filter List
+            List<Game> filteredGames = new List<Game>();
+            //Check Filters
+            CheckFilters(selectedFilter, filteredGames);
+        }
+
+        private void CheckFilters(string selectedFilter, List<Game> filteredGames)
+        {
+            switch (selectedFilter)
+            {
+                case "All":
+                    lbxGames.ItemsSource = null;
+                    lbxGames.ItemsSource = AllGames;
+                    Reset();
+                    break;
+                case "PC":
+                    lbxGames.ItemsSource = null;
+                    var queryPC = db.Games
+                        .Where(g => g.ID == 1)
+                        .Select(g => g);
+                    filteredGames = queryPC.ToList();
+                    lbxGames.ItemsSource = filteredGames;
+                    Reset();
+                    break;
+                case "PS":
+                    lbxGames.ItemsSource = null;
+                    var queryPS = db.Games
+                        .Where(g => g.Platform == "PS")
+                        .Select(g => g);
+                    filteredGames = queryPS.ToList();
+                    lbxGames.ItemsSource = filteredGames;
+                    Reset();
+                    break;
+                case "Switch":
+                    lbxGames.ItemsSource = null;
+                    var queryS = db.Games
+                        .Where(g => g.Platform == "Switch")
+                        .Select(g => g);
+                    filteredGames = queryS.ToList();
+                    lbxGames.ItemsSource = filteredGames;
+                    Reset();
+                    break;
+                case "Xbox":
+                    lbxGames.ItemsSource = null;
+                    var queryX = db.Games
+                        .Where(g => g.Platform == "Xbox")
+                        .Select(g => g);
+                    filteredGames = queryX.ToList();
+                    lbxGames.ItemsSource = filteredGames;
+                    Reset();
+                    break;
+            }
+        }
+
+        private void Reset()
+        {
+            lbxGames.SelectedItem = null;
+            tblkGameDetails.Text = "";
+            tblkGameDescription.Text = "";
         }
         #endregion
     }
